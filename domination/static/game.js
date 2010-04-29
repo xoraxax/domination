46511d4e2a9114eb238c0ab5b6379b9df0134218
Alexander Schremmer <alex AT alexanderweb DOT de@localhost>
2010-04-29 23:34:42 +0200

    Do not reload page when the modal dialog is opened.

--- domination/static/game.js
+++ domination/static/game.js
@@ -1,8 +1,9 @@
+do_page_refresh = true;
 handle_page_refresh = function () {
   var seqno = initial_seqno;
   var check_and_update = function() {
     $.getJSON($SCRIPT_ROOT + '/game/check_seqno/' + game_name, {seqno: seqno}, function(data) {
-      if (!(data === null)) {
+      if (!(data === null) && do_page_refresh) {
         document.location = document.location;
         window.setTimeout(check_and_update, 100);
       }
@@ -26,11 +27,12 @@ function your_turn_reminder() {
 
 _toggle_supply_lock = false;
 
-function toggle_supply() {
-  if (!_toggle_supply_lock) {
-    this._toggle_supply_lock = true;
-    $('div#supplycards').slideToggle(500, function() {
-      _toggle_supply_lock = false;
-    });
-  }
+function show_supply() {
+  do_page_refresh = false;
+  $('#supplycards').dialog({ modal: true, width: 700, title: 'Supply',
+      close: function(event, ui) {
+        do_page_refresh = true;
+        handle_page_refresh();
+      }
+  });
 }
