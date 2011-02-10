be2e0f8d7b33ea6de911d170f36a2f830b2b7f49
Timo Boettcher <domination@spida.net>
2011-02-10 13:27:57 +0100

    Fix bishop.

--- domination/cards/prosperity.py
+++ domination/cards/prosperity.py
@@ -30,7 +30,6 @@ class TradeRoute(ActionCard):
     def activate_action(self, game, player):
         player.remaining_deals += 1
         player.virtual_money += 1 #FIXME
-        pass
 
 class Loan(TreasureCard):
     name = _("Loan")
@@ -98,7 +97,19 @@ class Bishop(ActionCard):
             if cards:
                 card = cards[0]
                 player.tokens += card.cost / 2
-        pass
+                card.trash(game, player)
+                for info_player in game.following_participants(player):
+                    yield InfoRequest(game, info_player,
+                            _("%s trashes:", (player.name, )), [card])
+            for other_player in game.following_players(player):
+                cards = yield SelectHandCards(game, other_player, count_lower=0, count_upper=1,
+                        msg=_("%s played Bishop. You may trash a card:?", (player.name, )))
+                if cards:
+                    for card in cards:
+                        card.trash(game, other_player)
+                        for info_player in game.following_participants(other_player):
+                            yield InfoRequest(game, info_player,
+                                _("%s trashes:", (other_player.name, )), cards)
 
 class Monument(ActionCard):
     name = _("Monument")
